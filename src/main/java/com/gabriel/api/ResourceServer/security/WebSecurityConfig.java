a73b4c61f66cd3dd83ac6c62b98f8ff8970c6f34
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2020-11-23 19:47:05 +0100

    add SCOPE based authority

--- src/main/java/com/gabriel/api/ResourceServer/security/WebSecurityConfig.java
+++ src/main/java/com/gabriel/api/ResourceServer/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.gabriel.api.ResourceServer.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,14 +14,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        //((HttpSecurity)((HttpSecurity)((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)
-
                 http
-                        .authorizeRequests().anyRequest().authenticated()
+                        .authorizeRequests()
+                                .antMatchers(HttpMethod.GET, "/users/status/check").hasAuthority("SCOPE_profile")
+                        .anyRequest().authenticated()
                         .and()
                         .oauth2ResourceServer()
                         .jwt();
     }
+
 
 
 }
